4eb8597a2721d98bacd838e4e14179f7276872f4
AdrianBurke1 <adrianburke902@gmail.com>
2024-09-27 15:54:17 -0400

    something

--- models/User.js
+++ models/User.js
@@ -3,9 +3,15 @@ const { db, DataTypes } = require('../db/connection')
 
 // Creating a User child class from the Model parent class
 const User = db.define('users', {
-  username: DataTypes.STRING,
-  password: DataTypes.STRING
-})
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+});
 
 // exports
 module.exports = User
